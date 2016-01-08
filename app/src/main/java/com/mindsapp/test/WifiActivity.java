65245816348a7be72f6fa1023bbcadfe73d39dc6
Daniele <daniele.difratta@live.it>
2016-01-09 00:31:28 +0100

    Finito Service

--- app/src/main/java/com/mindsapp/test/WifiActivity.java
+++ app/src/main/java/com/mindsapp/test/WifiActivity.java
@@ -135,7 +135,7 @@ public class WifiActivity extends AppCompatActivity {
                 wifiManager.startScan();
             }
             while (!activityVisible){
-
+                //
             }
         }
 
